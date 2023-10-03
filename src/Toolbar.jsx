230fd3b9d4ecb5424b50febfc72b0d2edd22f87c
Mohamed <looklike882@gmail.com>
2023-10-03 22:14:30 +0100

    fix the display of toolbar

--- src/Toolbar.jsx
+++ src/Toolbar.jsx
@@ -1,7 +1,7 @@
 const Toolbar = () => {
   return (
     <div className="w-80 h-screen ">
-      <div className="flex flex-col justify-start gap-9">
+      <div className="flex flex-col justify-start gap-9 fixed">
         <button className=" w-56 bg-indigo-800     text-indigo-300  bg-opacity-60 rounded-full p-5  h-[4em]  flex flex-row justify-start  items-center gap-4      ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
--- src/sideBar.jsx
+++ src/sideBar.jsx
@@ -4,7 +4,7 @@ const SideBar = () => {
   return (
     <div className="w-2/4  h-screen border-b border-t border-stone-500 ">
       <BarChart getdata={null} />
-      <Habbits acivitiesNames={null} />
+        <Habbits acivitiesNames={null} />
     </div>
   );
 };
