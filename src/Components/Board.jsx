eed6e5f9ec541e1d29c0ff7878fbb34c1a5754b3
Mohamed <looklike882@gmail.com>
2023-10-29 23:14:08 +0100

    dragging done

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,33 +1,88 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { PlusIcon } from "../Icons/PlusIcon";
 import ColomnContainer from "./ColomnContainer";
-
+import { DndContext, DragOverlay, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
+import {SortableContext, arrayMove} from '@dnd-kit/sortable';
+import { createPortal } from "react-dom";
 export const Board = () => {
-          const [columns,setColumns]=useState([])
-          console.log(columns);
+  const [columns, setColumns] = useState([]);
+  const [Activecolumn, setActiveColumns] = useState();
+
+  const columnsId=useMemo(()=>columns.map((col)=>col.Id)
+  ,[columns])
+const sensors=useSensors(useSensor(PointerSensor,{
+          activationConstraint:{
+                    distance:3
+          }
+}))
 
   return (
     <div className=" m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px[40px]  ">
-      <div className="m-auto flex gap-4">
+      <DndContext sensors={sensors} onDragStart={OnDragStart} onDragEnd={OnDragEnd}>
+        <div className="m-auto flex gap-4">
           <div className="flex gap-4">
-                    {columns.map((col)=>(
-                              <ColomnContainer key={col.Title} column={col}/>
+            <SortableContext items ={columnsId}>
+                    {columns.map((col) => (
+                    <ColomnContainer
+                    key={col.Title}
+                    column={col}
+                    DeleteID={DeleteID}
+                    />
                     ))}
+            </SortableContext>
           </div>
-        <button onClick={()=>{CreateNewColumns()}} className="w-[360px] h-[60px] cursor-pointer min-w-[360px] rounded-lg bg-mainsBckground border-ColumnBckground ring-rose-500 border-2 p-4   hover:ring-2 flex gap-2">
-        <PlusIcon/> Add Column
-        </button>
-      </div>
+          <button
+            onClick={() => {
+              CreateNewColumns();
+            }}
+            className="w-[360px] h-[60px] cursor-pointer min-w-[360px] rounded-lg bg-mainsBckground border-ColumnBckground ring-rose-500 border-2 p-4   hover:ring-2 flex gap-2"
+          >
+            <PlusIcon /> Add Column
+          </button>
+        </div>
+        {createPortal(        <DragOverlay>
+          {
+                    Activecolumn && <ColomnContainer column={Activecolumn} DeleteID={DeleteID}/>
+          }
+        </DragOverlay>,document.body)}
+
+      </DndContext>
     </div>
   );
-  function CreateNewColumns(){
-          const NewColumn={
-                    Id:generateId(),
-                    Title:`Column ${columns.length + 1 }`
+  function CreateNewColumns() {
+    const NewColumn = {
+      Id: generateId(),
+      Title: `Column ${columns.length + 1}`,
+    };
+    setColumns([...columns, NewColumn]);
+  }
+  function DeleteID(Id) {
+    const FiltredArr = columns.filter((col) => col.Id !== Id);
+    setColumns(FiltredArr);
+  }
+  function OnDragStart(ev){
+          console.log(ev);
+          if(ev.active.data.current?.type==="column"){
+                    setActiveColumns(ev.active.data.current.column)
+
+
           }
-          setColumns([...columns,NewColumn])
-          }
+console.log(Activecolumn);
+return;
+  }
+  function OnDragEnd(e){
+          const {active,over}=e
+          if(!over)return;
+          const activeColumnId=active.id;
+          const OverColumnId=over.id
+          if(activeColumnId===OverColumnId)return;
+          setColumns((columns)=>{
+                    const ActivecolumnIndex=columns.findIndex((col)=>col.Id=== activeColumnId)
+                    const OvercolumnIndex=columns.findIndex((col)=>col.Id=== OverColumnId)
+                    return arrayMove(columns,ActivecolumnIndex,OvercolumnIndex)
+          })
+  }
 };
-function generateId(){
-          return Math.floor(Math.random()* 10001)
+function generateId() {
+  return Math.floor(Math.random() * 10001);
 }
--- src/Components/ColomnContainer.jsx
+++ src/Components/ColomnContainer.jsx
@@ -1,12 +1,51 @@
-import propTypes from "prop-types"
+import propTypes from "prop-types";
+import {useSortable} from '@dnd-kit/sortable';
+import { Delete } from "../Icons/Delete";
+import {CSS} from "@dnd-kit/utilities"
+const ColomnContainer = ({ column, DeleteID }) => {
+          const {setNodeRef,attributes,listeners,transform,transition,isDragging}=useSortable({
+                    id:column.Id,
+                    data:{
+                              type:"column",
+                              column
+                    }
+          })
+          const Style={
+                    transition,
+                    transform:CSS.Transform.toString(transform)
+          }
+          if(isDragging){
+                    return(
+                    <div ref={setNodeRef} style={Style} className=" w-[360px] h-[500px] max-h-[500px] border-2 border-rose-500 opacity-40  rounded-md flex flex-col">
+                    </div>);
 
-const ColomnContainer = ({column}) => {
-  return (
-    <div>{column.Title}</div>
-  )
-}
-ColomnContainer.propTypes={
-          column:propTypes.object.isRequired
-}
+          }
+          return (
+    <div ref={setNodeRef} style={Style} className="bg-black w-[360px] h-[500px] max-h-[500px] rounded-md flex flex-col">
+      <div {...attributes} {...listeners} className="bg-mainsBckground  text-lg font-bold h-[60px] cursor-grab rounded-md rounded-b-none p-3 border-black border-4 flex justify-between items-center">
+        <div className="flex gap-2">
+          <div className="flex justify-center items-center bg-ColumnBckground px-2 py-1 text-sm  ">
+            {column.Id}
+          </div>
+          {column.Title}
+        </div>
+        <button
+          onClick={() => {
+                    DeleteID(column.Id);
+          }}
+          className="stroke-gray-500 hover:stroke-cyan-50 hover:bg-ColumnBckground rounded p-2 px-1 py-2   "
+        >
+          <Delete />
+        </button>
+      </div>
+      <div className="flex flex-grow   ">content</div>
+      <div>footer</div>
+    </div>
+  );
+};
+ColomnContainer.propTypes = {
+  column: propTypes.object.isRequired,
+  DeleteID: propTypes.func.isRequired,
+};
 
-export default ColomnContainer
+export default ColomnContainer;
