bcf7303c16cb3c7cc6dc0394a72b5e68a7c2ead9
Mohamed <looklike882@gmail.com>
2023-10-31 22:11:46 +0100

    Input field fixing

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,34 +1,47 @@
 import { useMemo, useState } from "react";
 import { PlusIcon } from "../Icons/PlusIcon";
 import ColomnContainer from "./ColomnContainer";
-import { DndContext, DragOverlay, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
-import {SortableContext, arrayMove} from '@dnd-kit/sortable';
+import {
+  DndContext,
+  DragOverlay,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
 import { createPortal } from "react-dom";
 export const Board = () => {
   const [columns, setColumns] = useState([]);
   const [Activecolumn, setActiveColumns] = useState();
 
-  const columnsId=useMemo(()=>columns.map((col)=>col.Id)
-  ,[columns])
-const sensors=useSensors(useSensor(PointerSensor,{
-          activationConstraint:{
-                    distance:3
-          }
-}))
+  const columnsId = useMemo(() => columns.map((col) => col.Id), [columns]);
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 3,
+      },
+    }),
+  );
 
   return (
     <div className=" m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px[40px]  ">
-      <DndContext sensors={sensors} onDragStart={OnDragStart} onDragEnd={OnDragEnd}>
+      <DndContext
+        sensors={sensors}
+        onDragStart={OnDragStart}
+        onDragEnd={OnDragEnd}
+      >
         <div className="m-auto flex gap-4">
           <div className="flex gap-4">
-            <SortableContext items ={columnsId}>
-                    {columns.map((col) => (
-                    <ColomnContainer
-                    key={col.Title}
-                    column={col}
-                    DeleteID={DeleteID}
-                    />
-                    ))}
+            <SortableContext items={columnsId}>
+              {columns.map((col) => (
+                <ColomnContainer
+                  key={col.Title}
+                  column={col}
+                  DeleteID={DeleteID}
+                  UpadateTitle={UpadateTitle}
+
+                />
+              ))}
             </SortableContext>
           </div>
           <button
@@ -40,12 +53,18 @@ const sensors=useSensors(useSensor(PointerSensor,{
             <PlusIcon /> Add Column
           </button>
         </div>
-        {createPortal(        <DragOverlay>
-          {
-                    Activecolumn && <ColomnContainer column={Activecolumn} DeleteID={DeleteID}/>
-          }
-        </DragOverlay>,document.body)}
-
+        {createPortal(
+          <DragOverlay>
+            {Activecolumn && (
+              <ColomnContainer
+                column={Activecolumn}
+                DeleteID={DeleteID} 
+                UpadateTitle={UpadateTitle}
+              />
+            )}
+          </DragOverlay>,
+          document.body,
+        )}
       </DndContext>
     </div>
   );
@@ -60,28 +79,40 @@ const sensors=useSensors(useSensor(PointerSensor,{
     const FiltredArr = columns.filter((col) => col.Id !== Id);
     setColumns(FiltredArr);
   }
-  function OnDragStart(ev){
-          console.log(ev);
-          if(ev.active.data.current?.type==="column"){
-                    setActiveColumns(ev.active.data.current.column)
+  function UpadateTitle(id,Title) {
+    const EditedCol = columns.map(col => {
+    if (col.Id !== id) return col;
+      return { ...col, Title };
+    });
+    setColumns(EditedCol);
 
+  }
+  function OnDragStart(ev) {
+    console.log(ev);
+    if (ev.active.data.current?.type === "column") {
+      setActiveColumns(ev.active.data.current.column);
+    }
+    console.log(Activecolumn);
+    return;
+  }
+  function OnDragEnd(e) {
+    const { active, over } = e;
+    if (!over) return;
+    const activeColumnId = active.id;
+    const OverColumnId = over.id;
+    if (activeColumnId === OverColumnId) return;
+    setColumns((columns) => {
+      const ActivecolumnIndex = columns.findIndex(
+        (col) => col.Id === activeColumnId,
+      );
+      const OvercolumnIndex = columns.findIndex(
+        (col) => col.Id === OverColumnId,
+      );
+      return arrayMove(columns, ActivecolumnIndex, OvercolumnIndex);
+    });
+  }
+  
 
-          }
-console.log(Activecolumn);
-return;
-  }
-  function OnDragEnd(e){
-          const {active,over}=e
-          if(!over)return;
-          const activeColumnId=active.id;
-          const OverColumnId=over.id
-          if(activeColumnId===OverColumnId)return;
-          setColumns((columns)=>{
-                    const ActivecolumnIndex=columns.findIndex((col)=>col.Id=== activeColumnId)
-                    const OvercolumnIndex=columns.findIndex((col)=>col.Id=== OverColumnId)
-                    return arrayMove(columns,ActivecolumnIndex,OvercolumnIndex)
-          })
-  }
 };
 function generateId() {
   return Math.floor(Math.random() * 10001);
--- src/Components/ColomnContainer.jsx
+++ src/Components/ColomnContainer.jsx
@@ -1,43 +1,74 @@
 import propTypes from "prop-types";
-import {useSortable} from '@dnd-kit/sortable';
+import { useSortable } from "@dnd-kit/sortable";
 import { Delete } from "../Icons/Delete";
-import {CSS} from "@dnd-kit/utilities"
+import { CSS } from "@dnd-kit/utilities";
 import { useState } from "react";
-const ColomnContainer = ({ column, DeleteID }) => {
-          const [editMode,seteditMode]=useState(false)
-          const {setNodeRef,attributes,listeners,transform,transition,isDragging}=useSortable({
-                    id:column.Id,
-                    data:{
-                              type:"column",
-                              column
-                    }
-          })
-          const Style={
-                    transition,
-                    transform:CSS.Transform.toString(transform)
-          }
-          if(isDragging){
-                    return(
-                    <div ref={setNodeRef} style={Style} className=" w-[360px] h-[500px] max-h-[500px] border-2 border-rose-500 opacity-40  rounded-md flex flex-col">
-                    </div>);
-
-          }
-          return (
-    <div ref={setNodeRef} style={Style} className="bg-black w-[360px] h-[500px] max-h-[500px] rounded-md flex flex-col">
-      <div {...attributes} {...listeners} className="bg-mainsBckground  text-lg font-bold h-[60px] cursor-grab rounded-md rounded-b-none p-3 border-black border-4 flex justify-between items-center" onClick={()=>{seteditMode(true)} }>
+const ColomnContainer = ({ column, DeleteID, UpadateTitle }) => {
+  const [editMode, seteditMode] = useState(false);
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.Id,
+    data: {
+      type: "column",
+      column,
+    },
+    disabled: editMode,
+  });
+  const Style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={Style}
+        className=" w-[360px] h-[500px] max-h-[500px] border-2 border-rose-500 opacity-40  rounded-md flex flex-col"
+      ></div>
+    );
+  }
+  return (
+    <div
+      ref={setNodeRef}
+      style={Style}
+      className="bg-black w-[360px] h-[500px] max-h-[500px] rounded-md flex flex-col"
+    >
+      <div
+        {...attributes}
+        {...listeners}
+        className="bg-mainsBckground  text-lg font-bold h-[60px] cursor-grab rounded-md rounded-b-none p-3 border-black border-4 flex justify-between items-center"
+        onClick={() => {
+          seteditMode(true);
+        }}
+      >
         <div className="flex gap-2">
           <div className="flex justify-center items-center bg-ColumnBckground px-2 py-1 text-sm  ">
             {column.Id}
           </div>
-           { !editMode && column.Title}
-           {editMode && <input type="text" autoFocus onBlur={()=>{seteditMode(false)}} onKeyDown={(e)=>{
-            if(e.key==="Entre") return;
-            seteditMode(false)
-           }}/>  }
+          {!editMode && column.Title}
+          {editMode && (
+            <input value={column.Title}
+              onChange={(e) => UpadateTitle(column.Id,e.target.value)}
+              autoFocus
+              className="bg-ColumnBckground w-full focus:border-rose-500 border rounded-lg outline-none px-2 "
+              onBlur={() =>seteditMode(false)
+              }
+               onKeyDown={(e)=>{
+                if(e.key =="Entre") return;
+                seteditMode(false)
+               }}
+            />
+          )}
         </div>
         <button
           onClick={() => {
-                    DeleteID(column.Id);
+            DeleteID(column.Id);
           }}
           className="stroke-gray-500 hover:stroke-cyan-50 hover:bg-ColumnBckground rounded p-2 px-1 py-2   "
         >
@@ -52,6 +83,7 @@ const ColomnContainer = ({ column, DeleteID }) => {
 ColomnContainer.propTypes = {
   column: propTypes.object.isRequired,
   DeleteID: propTypes.func.isRequired,
+  UpadateTitle: propTypes.func.isRequired
 };
 
 export default ColomnContainer;
